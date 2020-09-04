3c0aa2b84831fedaab5e1101e258aebabc1bfd87
Castiel Yu <yuche119@gmail.com>
2020-09-04 08:02:28 +0800

    add TAIWAN

--- driving.py
+++ driving.py
@@ -7,3 +7,9 @@ if country == '台灣':
         print('您可以考駕照')
     else:
         print('您不可以考駕照')
+
+elif country == '美國':
+        if age >= 16:
+        print('您可以考駕照')
+    else:
+        print('您不可以考駕照')
